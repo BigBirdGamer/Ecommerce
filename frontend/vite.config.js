1f1ab06d0a02b1bd5b6a3517ed2e33d6d3d785f7
BigBirdGamer <87754100+BigBirdGamer@users.noreply.github.com>
2022-08-12 08:41:11 +0800

    Update vit config files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    origin: 'localhost:3000',
     port: 3001,
     proxy: { "^/api/.*" : "http://localhost:3000/" },
   }
